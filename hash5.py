1bfe6cc1e8af9fd4dc00f28270e1e5d3600f96fe
Shreyas Walmik Ghodake <140911851+shreyas-codes25@users.noreply.github.com>
2025-05-08 10:17:50 +0530

    Add files via upload

---
+++ hash5.py
@@ -0,0 +1,12 @@
+import hashlib
+print("Enter the msg:")
+p = input() 
+result = hashlib.md5(p.encode()).hexdigest()  
+print("The equivalent hexadecimal value:",end="")
+print(result)
+result2 = hashlib.sha256(p.encode()).hexdigest()  
+print("The equivalent hexadecimal value:",end="")
+print(result2)
+result1 = hashlib.sha384(p.encode()).hexdigest() 
+print("The equivalent hexadecimal value:",end="")
+print(result1)
